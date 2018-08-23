54772bc00dbb2c9614f97b3a3601644bdcf5b047
Shradhey Tripathi <shradhey029tripathi@gmail.com>
2018-08-23 23:43:35 +0530

    Added fix for search issue

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import CardList from './CardList';
 import SearchBox from './SearchBox';
 import { robots } from './robots';
-
+import './App.css'
 
 class App extends Component {
   constructor() {
@@ -14,19 +14,18 @@ class App extends Component {
   }
 
   onSearchChange = (event) => {
-    console.log(event.target.value);
-    const filteredRobots = this.state.robots.filter(robots => {
-      return robots.name.toLowerCase().includes(this.searchfield.toLowerCase());
-    });
-    console.log(filteredRobots);
+    this.setState({ searchfield: event.target.value });
   }
 
   render() {
+    const filteredRobots = this.state.robots.filter(robots => {
+      return robots.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
+    });
     return (
       <div className='tc'>
-        <h1> RoboFriends</h1>
+        <h1 className='f1'> RoboFriends</h1>
         <SearchBox searchChange={this.onSearchChange} />
-        <CardList robots={this.state.robots} />
+        <CardList robots={filteredRobots} />
       </div>
 
     );
